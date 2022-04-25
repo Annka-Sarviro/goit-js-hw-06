516f98dba9983f90f191b7831f6c7415800ad80c
Ann Sarviro <Annka_nuha@i.ua>
2022-04-26 01:00:10 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,12 +1,32 @@
 const refs = {
-  divControlsEl: document.querySelector("#controls"),
+  divBoxesEl: document.querySelector("#boxes"),
   inputEl: document.querySelector("#controls input"),
   buttonCreateEl: document.querySelector("[data-create]"),
   buttonDestroyEl: document.querySelector("[data-destroy]"),
 };
 
 
+refs.buttonCreateEl.addEventListener('click', () => createBoxes(refs.inputEl.value));
+refs.buttonDestroyEl.addEventListener('click', destroyBoxes);
 
+function createBoxes(amount) {
+  const array = [];
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${boxSize}px`;
+      box.style.height = `${boxSize}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      array.push(box);
+      boxSize += 10;
+  } 
+  refs.divBoxesEl.append(...array)
+}
+
+
+function  destroyBoxes() {
+    refs.divBoxesEl.innerHTML = '';
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
